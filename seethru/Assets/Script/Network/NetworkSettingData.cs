9ee5d1c235e1781875461be7405f66835838e7d3
TakutoJin <takutojinwork@gmail.com>
2019-10-11 16:10:54 +0900

    一人のみ接続に対応（？）

--- seethru/Assets/Script/Network/NetworkSettingData.cs
+++ seethru/Assets/Script/Network/NetworkSettingData.cs
@@ -3,21 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using MrsServer = System.IntPtr;
-using MrsConnection = System.IntPtr;
-using MrsCipher = System.IntPtr;
-
-public class NetworkSettingData : Mrs
+public class NetworkSettingData
 {
-    public static string addr;
-    public static string port;
-    public static MrsConnection connection;
+    public string addr;
+    public ushort port;
+    public IntPtr connection;
 
-    public void SetData(string _addr, string _port)
-    {
-        addr = _addr;
-        port = _port;
-    }
+    
 
     static NetworkSettingData()
     {
