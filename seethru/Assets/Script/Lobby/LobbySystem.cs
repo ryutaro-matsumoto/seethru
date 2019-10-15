b4e3fb0e06c9afe6db084d3fe9c54f58f4269639
ren.matsu <ren.matsugasako@gmail.com>
2019-10-15 11:12:58 +0900

    タイトル追加

---
+++ seethru/Assets/Script/Lobby/LobbySystem.cs
@@ -0,0 +1,76 @@
+﻿//------------------------------------------------------------------------------
+// @name：LobbySystem.cs
+//
+// @note：
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// namespace declaration.
+//------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//------------------------------------------------------------------------------
+// LobbySystem class.
+//------------------------------------------------------------------------------
+public class LobbySystem : MonoBehaviour
+{
+    //\ 戻るSE
+    public AudioSource ClickBackSound;
+    public  GameObject RoomCreate;
+    public  GameObject RoomJoin;
+    public  GameObject Back;
+
+    //------------------------------------------------------------------------------
+    // start function.
+    //------------------------------------------------------------------------------
+    void Start()
+    {
+        FadeManeger.Fadein();
+
+    }
+
+    //------------------------------------------------------------------------------
+    // update function.
+    //------------------------------------------------------------------------------
+    void Update()
+    {
+     
+    }
+
+
+    //===========================================================
+    // ClickCreateRoomButton function.
+    //
+    // @note：「ルームを作成」ボタンをクリック時処理
+    //===========================================================
+    public void ClickCreateRoomButton()
+    {
+        RoomCreate.SetActive(true);
+        RoomJoin.SetActive(false);
+    }
+
+    //===========================================================
+    // ClickJoinRoomButton function.
+    //
+    // @note：「ルームに入る」ボタンをクリック時処理
+    //===========================================================
+    public void ClickJoinRoomButton()
+    {
+        RoomCreate.SetActive(false);
+        RoomJoin.SetActive(true);
+    }
+
+    //===========================================================
+    // ClickBackButton function.
+    //
+    // @note：「戻る」ボタンをクリック時処理
+    //===========================================================
+    public void ClickBackButton()
+    {
+        ClickBackSound.PlayOneShot(ClickBackSound.clip);
+        FadeManeger.Fadeout("TitleScene");
+    }
+}
---
+++ seethru/Assets/Script/Title/TitleSystem.cs
@@ -0,0 +1,70 @@
+﻿//------------------------------------------------------------------------------
+// @name：TitleSystem.cs
+//
+// @note：
+//------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//------------------------------------------------------------------------------
+// TitleSystem class.
+//------------------------------------------------------------------------------
+public class TitleSystem : MonoBehaviour
+{
+    public AudioSource ClicjGameStartSound;
+
+    //------------------------------------------------------------------------------
+    // start function.
+    //------------------------------------------------------------------------------
+    void Start()
+    {
+        FadeManeger.Fadein();
+    }
+
+    //------------------------------------------------------------------------------
+    // update function.
+    //------------------------------------------------------------------------------
+    void Update()
+    {
+
+    }
+
+    //===========================================================
+    // Quit function.
+    //
+    // @note：ゲーム終了
+    //===========================================================
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+        UnityEngine.Application.Quit();
+#endif
+    }
+
+
+    //===========================================================
+    // ClickGameStartButton function.
+    //
+    // @note：「Game Start」ボタンをクリック時処理
+    //===========================================================
+    public void ClickGameStartButton()
+    {
+        ClicjGameStartSound.PlayOneShot(ClicjGameStartSound.clip);
+        FadeManeger.Fadeout("ProtoScene");
+    }
+
+    //===========================================================
+    // ClickExitButton function.
+    //
+    // @note：「Exit」ボタンをクリック時処理
+    //===========================================================
+    public void ClickExitButton()
+    {
+        Quit();
+    }
+
+}
