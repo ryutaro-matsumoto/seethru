8e39416365d271a00200fc449096b8d72a1eedc8
TakutoJin <takutojinwork@gmail.com>
2019-10-15 16:19:39 +0900

    ProtoSceneのInitに組み込んでもらうInitMrsforGame()の追加。受信した弾の情報の反映バグ修正

--- seethru/Assets/Script/Network/MrsClient.cs
+++ seethru/Assets/Script/Network/MrsClient.cs
@@ -167,6 +167,13 @@ public class MrsClient : Mrs {
         }
     }
 
+    public void InitMrsforGame()
+    {
+        g_Object = GameObject.Find("MainPlayer");
+        g_EnemyObject = GameObject.Find("Player");
+        
+    }
+
     private void InitMyData()
     {
         myData.x = 0;
@@ -221,7 +228,7 @@ public class MrsClient : Mrs {
         }break;
             case 0x02:
                 {
-                    MRS_LOG_DEBUG("RECEIVED DATA:{0}", payload);
+                    //MRS_LOG_DEBUG("RECEIVED DATA:{0}", payload);
                     S_DataPlayer data = (S_DataPlayer)Marshal.PtrToStructure(payload, typeof(S_DataPlayer));
                     if (g_EnemyObject != null)
                     {
@@ -237,8 +244,8 @@ public class MrsClient : Mrs {
                     S_DataShots data = (S_DataShots)Marshal.PtrToStructure(payload, typeof(S_DataShots));
                     GameObject bulletPool = GameObject.Find("BulletPool");
                     Pool script = bulletPool.GetComponent<Pool>();
-                    
-                    script.Place(new Vector2(data.x, data.y), Quaternion.AngleAxis(data.angle,Vector3.forward));
+
+                    script.Place(new Vector2(data.x, data.y), Quaternion.AngleAxis(data.angle, Vector3.forward));
                     //MRS_LOG_DEBUG("RECEIVED DATA  pos_x:{0} pos_y:{1} pos_z:{2} angle:{3}",
                     //    data.pos_x, data.pos_y, data.pos_z, data.angle);
                 }
@@ -447,8 +454,11 @@ public class MrsClient : Mrs {
         mrs_update();
         if (g_gameon)
         {
+            // ProtoSceneの初期化でInitMrsforGame()が呼び出されれば不要
             if(g_Object == null) { g_Object = GameObject.Find("MainPlayer"); }
             if (g_EnemyObject == null) { g_EnemyObject = GameObject.Find("Player"); }
+
+
             CompareMyData();
         }
         if (connected && !g_gameon) {
