57c9108ab60e28159463de41b58b5533d76d52e7
松本龍太郎 <ryutaro.matsumoto0219@gmail.com>
2019-10-16 12:38:12 +0900

    GameManager不具合修正、ステージのイニシャライザを追加、遷移テスト用シーン追加

---
+++ seethru/Assets/Script/Proto/GameInitProto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitProto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		GameManager.ReceiveID(0);
+		uint[] table = { 0, 1, 2, 3 };
+		GameManager.ProtoStart(table, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
