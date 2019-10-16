f6eb19e9786da3b3df5d4138a691f892649b3601
松本龍太郎 <ryutaro.matsumoto0219@gmail.com>
2019-10-16 12:51:45 +0900

    カメラ追従バグ修正、生成したプレイヤーの名前変更（自分のIDがわかるように）

--- seethru/Assets/Script/Object/ButtleInitializer.cs
+++ seethru/Assets/Script/Object/ButtleInitializer.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class ButtleInitializer : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
 		GameManager.PlayersInit();
+
+		for(int i = 0; i < GameManager.playerNum; ++i){
+			if(GameManager.playID == i){
+				GameManager.players[i].name = "myplayer" + i;
+				continue;
+			}
+			GameManager.players[i].name = "player" + i;
+		}
     }
 
 }
