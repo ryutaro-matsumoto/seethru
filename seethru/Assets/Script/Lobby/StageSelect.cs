5281286fc2b61866c3f93d791e3e7dba6c92f09d
松本龍太郎 <ryutaro.matsumoto0219@gmail.com>
2019-10-23 18:05:13 +0900

    ロビーのUIの変更、ステージセレクトシステムを実装

---
+++ seethru/Assets/Script/Lobby/StageSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelect : MonoBehaviour
+{
+	public int stageMax = 1;
+
+	[SerializeField]
+	private int selectStage = 1;
+
+
+	private void Start() {
+		stageMax = transform.GetChild(0).childCount;
+	}
+
+	public void StageNumAdd(){
+		transform.GetChild(0).GetChild(selectStage - 1).gameObject.SetActive(false);
+		selectStage++;
+		if(selectStage > stageMax){
+			selectStage = 1;
+		}
+		transform.GetChild(0).GetChild(selectStage - 1).gameObject.SetActive(true);
+	}
+
+	public void StageNumSub() {
+		transform.GetChild(0).GetChild(selectStage - 1).gameObject.SetActive(false);
+		selectStage--;
+		if (selectStage < 1) {
+			selectStage = stageMax;
+		}
+		transform.GetChild(0).GetChild(selectStage - 1).gameObject.SetActive(true);
+	}
+}
