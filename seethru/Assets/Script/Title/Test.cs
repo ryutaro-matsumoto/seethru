23f87ec97a8fd3a8264e4617e611194161aade79
ren.matsu <ren.matsugasako@gmail.com>
2019-10-24 16:03:32 +0900

    パーティクル

---
+++ seethru/Assets/Script/Title/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public float speed = 3.0f;
+    Vector3 worldPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = worldPos;
+        }
+    }
+}
